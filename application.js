7a1c54f779d8c8e701d9cc3dcf27a0b9b6114465
Javier Collado <javier.collado@gmail.com>
2013-10-26 08:02:44 +0200

    Added new game button and question number

--- application.js
+++ application.js
@@ -1,68 +1,118 @@
 $(document).ready(function() {
-    Quiz.initialize([
+    Quiz([
         new Question("First question", ["a", "b", "c", "d"], 0),
         new Question("Second question", ["a", "b", "c", "d"], 0),
         new Question("Third question", ["a", "b", "c", "d"], 0),
         new Question("Fourth question", ["a", "b", "c", "d"], 0),
         new Question("Fifth question", ["a", "b", "c", "d"], 0)
-    ]);
-
-    $("form").submit(Quiz.handleSubmit);
-    Quiz.displayNextQuestion();
+    ]).onReady();
 });
 
-var Quiz = {
-    // Set questions to be displayed to the user
-    initialize: function(questions) {
-        Quiz.questions = questions;
-    },
+var Quiz = function(questions) {
+    var currentQuestionIndex = 0;
+    var currentQuestion = questions[currentQuestionIndex];
+    var $question = $("#question");
+    var $questionIndex = $question.find("#question_index");
+    var $questionCount = $question.find("#question_count");
+    var $summary = $("#summary");
+    var $newGame = $("#new_game");
 
-    // Display next question (if available)
-    displayNextQuestion: function() {
-        if (Quiz.questions.length > 0) {
-            var nextQuestion = Quiz.questions.shift();
-            nextQuestion.display();
-        }
-    },
+    var displayCurrentQuestion = function() {
+        currentQuestion = questions[currentQuestionIndex];
 
-    handleSubmit: function(event) {
+        $questionIndex.text(currentQuestionIndex + 1);
+
+        console.log("Displaying question...");
+        console.log(currentQuestion);
+        currentQuestion.display();
+    };
+
+    var displaySummary = function() {
+        console.log("Displaying summary...");
+        $question.toggle();
+        $summary.toggle();
+    };
+
+    var onAnswerClick = function() {
+        var selectedIndex = $(this).val();
+        console.log("Selected index: " + selectedIndex);
+        currentQuestion.selectedIndex = selectedIndex;
+    };
+
+    var onQuestionSubmit = function(event) {
+        // Form is never submitted to the server
         event.preventDefault();
 
-        console.log("Trying to submit...");
-        Quiz.displayNextQuestion();
-    }
-}
+        if (currentQuestion.isCorrect()) {
+            console.log("Right answer!");
+        } else {
+            console.log("Wrong answer!");
+        }
 
-function Question(question, choices, correctIndex) {
+        currentQuestionIndex += 1;
+        if (currentQuestionIndex < questions.length) {
+            displayCurrentQuestion();
+        } else {
+            displaySummary();
+        }
+    };
+
+    var onNewGameClick = function() {
+        $question.toggle();
+        $summary.toggle();
+
+        // Display first question again
+        currentQuestionIndex = 0;
+        displayCurrentQuestion();
+    };
+
+    return {
+        onReady: function() {
+            $questionCount.text(questions.length);
+            $question.on("click", "input[type=radio]", onAnswerClick);
+            $question.submit(onQuestionSubmit);
+            $newGame.click(onNewGameClick);
+
+            displayCurrentQuestion();
+        }
+    };
+};
+
+function Question(question, choices, correctIndex, selectedIndex) {
     this.question = question;
     this.choices = choices;
     this.correctIndex = correctIndex;
+    this.selectedIndex = selectedIndex || 0;
 }
 
 // Display question and available choices
 Question.prototype.display = function() {
     this.clear();
 
-    $("#question").text(this.question);
+    $("#question_text").text(this.question);
 
-    var ul = $("#choices");
-    for (index in this.choices) {
+    var ul = $("#question_choices");
+    for (var index in this.choices) {
         var choice = this.choices[index];
         var li = $(
            "<li>" +
            "<input type=\"radio\" " +
                   "name=\"choice\" " +
-                  "value=\"" + choice + "\"" +
-                  ((index == 0) ? "checked" : "") +
+                  "value=\"" + index + "\"" +
+                  ((index == this.selectedIndex) ? "checked" : "") +
                   ">" +
            choice +
            "</li>");
         li.appendTo(ul);
     }
-}
+};
 
 // Clear areas with question related values
 Question.prototype.clear = function() {
-    $("#question").text("");
-    $("#choices").empty();
-}
+    $("#question_text").text("");
+    $("#question_choices").empty();
+};
+
+Question.prototype.isCorrect = function() {
+    return this.correctIndex == this.selectedIndex;
+};
